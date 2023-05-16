4eb7d27e65341b5db41230488c6a859b7583cfa6
mxngocqb <87193435+mxngocqb@users.noreply.github.com>
2023-05-16 08:34:14 +0700

    gì

---
+++ fork.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main()
+{
+    while (1)
+    {
+        fork();
+        fork();
+        fork();
+        printf("Hello\n");
+    }
+}
